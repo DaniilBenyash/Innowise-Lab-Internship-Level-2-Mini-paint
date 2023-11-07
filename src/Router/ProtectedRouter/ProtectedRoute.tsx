e7a81af3e59a9909643a25b53340d143d81e43fd
Daniil Beniash <daniilbeniash97@gmail.com>
2023-11-07 09:29:41 +0300

    fix: ProtectedRouter

--- src/Router/ProtectedRouter/ProtectedRoute.tsx
+++ src/Router/ProtectedRouter/ProtectedRoute.tsx
@@ -1,14 +1,13 @@
 import { Navigate } from 'react-router'
 import { useUser } from '@/features/user/useUser'
 import { SIGN_IN } from '@/variables/routes'
-import { ReactNode } from 'react'
 
 type ProtectedRouteProps = {
-  children: ReactNode
+  children: JSX.Element
 }
 
 export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const { user } = useUser()
 
-  return (user.user ? children : <Navigate to={SIGN_IN} />) as JSX.Element
+  return (user.user ? children : <Navigate to={SIGN_IN} />)
 }
